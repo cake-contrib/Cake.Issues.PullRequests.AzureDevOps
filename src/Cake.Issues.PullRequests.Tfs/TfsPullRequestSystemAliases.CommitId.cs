15def62f4de2ebdc2fb1312a354e13c2328e5fb7
Pascal Berger <pascal.berger@gmail.com>
2018-08-19 21:07:10 +0200

    Split aliases into partial classes

---
+++ src/Cake.Issues.PullRequests.Tfs/TfsPullRequestSystemAliases.CommitId.cs
@@ -0,0 +1,48 @@
+﻿namespace Cake.Issues.PullRequests.Tfs
+{
+    using Core;
+    using Core.Annotations;
+
+    /// <content>
+    /// Contains functionality related to commit IDs.
+    /// </content>
+    public static partial class TfsPullRequestSystemAliases
+    {
+        /// <summary>
+        /// Gets the last commit hash on the source branch of the Team Foundation Server or
+        /// Visual Studio Team Services pull request using the specified settings.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="settings">Settings for accessing the pull request system.</param>
+        /// <example>
+        /// <para>Get the hash of the last commit:</para>
+        /// <code>
+        /// <![CDATA[
+        ///     var pullRequestSettings =
+        ///         new TfsPullRequestSettings(
+        ///             new Uri("http://myserver:8080/tfs/defaultcollection/myproject/_git/myrepository"),
+        ///             "refs/heads/feature/myfeature",
+        ///             TfsAuthenticationNtlm());
+        ///
+        ///     var hash =
+        ///         TfsPullRequestLastSourceCommit(
+        ///             pullRequestSettings);
+        /// ]]>
+        /// </code>
+        /// </example>
+        /// <returns>The hash of the last commit on the source branch or <see cref="string.Empty"/>
+        /// if no pull request could be found.</returns>
+        [CakeMethodAlias]
+        [CakeAliasCategory(PullRequestsAliasConstants.PullRequestSystemCakeAliasCategory)]
+        public static string TfsPullRequestLastSourceCommit(
+            this ICakeContext context,
+            TfsPullRequestSettings settings)
+        {
+            context.NotNull(nameof(context));
+            settings.NotNull(nameof(settings));
+
+            var pullRequestSystem = new TfsPullRequestSystem(context.Log, settings);
+            return pullRequestSystem.GetCapability<TfsCheckingCommitIdCapability>().GetLastSourceCommitId();
+        }
+    }
+}
---
+++ src/Cake.Issues.PullRequests.Tfs/TfsPullRequestSystemAliases.PullRequestSystem.cs
@@ -0,0 +1,139 @@
+﻿namespace Cake.Issues.PullRequests.Tfs
+{
+    using System;
+    using Core;
+    using Core.Annotations;
+
+    /// <content>
+    /// Contains functionality related to <see cref="TfsPullRequestSystem"/>.
+    /// </content>
+    public static partial class TfsPullRequestSystemAliases
+    {
+        /// <summary>
+        /// Gets an object for writing issues to Team Foundation Server or Visual Studio Team Services pull request
+        /// in a specific repository and for a specific source branch.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="repositoryUrl">Full URL of the Git repository,
+        /// eg. <code>http://myserver:8080/tfs/defaultcollection/myproject/_git/myrepository</code>.
+        /// Supported URL schemes are HTTP, HTTPS and SSH.
+        /// URLs using SSH scheme are converted to HTTPS.</param>
+        /// <param name="sourceBranch">Branch for which the pull request is made.</param>
+        /// <param name="credentials">Credentials to use to authenticate against Team Foundation Server or
+        /// Visual Studio Team Services.</param>
+        /// <returns>Object for writing issues to Team Foundation Server or Visual Studio Team Services pull request.</returns>
+        /// <example>
+        /// <para>Report code analysis issues reported as MsBuild warnings to a TFS pull request:</para>
+        /// <code>
+        /// <![CDATA[
+        ///     ReportIssuesToPullRequest(
+        ///         MsBuildCodeAnalysis(
+        ///             @"c:\build\msbuild.log",
+        ///             MsBuildXmlFileLoggerFormat),
+        ///         TfsPullRequests(
+        ///             new Uri("http://myserver:8080/tfs/defaultcollection/myproject/_git/myrepository"),
+        ///             "refs/heads/feature/myfeature",
+        ///             TfsAuthenticationNtlm()),
+        ///         @"c:\repo");
+        /// ]]>
+        /// </code>
+        /// </example>
+        [CakeMethodAlias]
+        [CakeAliasCategory(PullRequestsAliasConstants.PullRequestSystemCakeAliasCategory)]
+        public static IPullRequestSystem TfsPullRequests(
+            this ICakeContext context,
+            Uri repositoryUrl,
+            string sourceBranch,
+            ITfsCredentials credentials)
+        {
+            context.NotNull(nameof(context));
+            repositoryUrl.NotNull(nameof(repositoryUrl));
+            sourceBranch.NotNullOrWhiteSpace(nameof(sourceBranch));
+            credentials.NotNull(nameof(credentials));
+
+            return context.TfsPullRequests(new TfsPullRequestSettings(repositoryUrl, sourceBranch, credentials));
+        }
+
+        /// <summary>
+        /// Gets an object for writing issues to Team Foundation Server or Visual Studio Team Services pull request
+        /// in a specific repository and with a specific ID.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="repositoryUrl">Full URL of the Git repository,
+        /// eg. <code>http://myserver:8080/tfs/defaultcollection/myproject/_git/myrepository</code>.
+        /// Supported URL schemes are HTTP, HTTPS and SSH.
+        /// URLs using SSH scheme are converted to HTTPS.</param>
+        /// <param name="pullRequestId">ID of the pull request.</param>
+        /// <param name="credentials">Credentials to use to authenticate against Team Foundation Server or
+        /// Visual Studio Team Services.</param>
+        /// <returns>Object for writing issues to Team Foundation Server or Visual Studio Team Services pull request.</returns>
+        /// <example>
+        /// <para>Report code analysis issues reported as MsBuild warnings to a TFS pull request:</para>
+        /// <code>
+        /// <![CDATA[
+        ///     ReportIssuesToPullRequest(
+        ///         MsBuildCodeAnalysis(
+        ///             @"c:\build\msbuild.log",
+        ///             MsBuildXmlFileLoggerFormat),
+        ///         TfsPullRequests(
+        ///             new Uri("http://myserver:8080/tfs/defaultcollection/myproject/_git/myrepository"),
+        ///             5,
+        ///             TfsAuthenticationNtlm()),
+        ///         @"c:\repo");
+        /// ]]>
+        /// </code>
+        /// </example>
+        [CakeMethodAlias]
+        [CakeAliasCategory(PullRequestsAliasConstants.PullRequestSystemCakeAliasCategory)]
+        public static IPullRequestSystem TfsPullRequests(
+            this ICakeContext context,
+            Uri repositoryUrl,
+            int pullRequestId,
+            ITfsCredentials credentials)
+        {
+            context.NotNull(nameof(context));
+            repositoryUrl.NotNull(nameof(repositoryUrl));
+            credentials.NotNull(nameof(credentials));
+
+            return context.TfsPullRequests(new TfsPullRequestSettings(repositoryUrl, pullRequestId, credentials));
+        }
+
+        /// <summary>
+        /// Gets an object for writing issues to Team Foundation Server or Visual Studio Team Services pull request
+        /// using the specified settings.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="settings">Settings for accessing the pull request system.</param>
+        /// <returns>Object for writing issues to Team Foundation Server or Visual Studio Team Services pull request.</returns>
+        /// <example>
+        /// <para>Report code analysis issues reported as MsBuild warnings to a TFS pull request:</para>
+        /// <code>
+        /// <![CDATA[
+        ///     var pullRequestSettings =
+        ///         new TfsPullRequestSettings(
+        ///             new Uri("http://myserver:8080/tfs/defaultcollection/myproject/_git/myrepository"),
+        ///             "refs/heads/feature/myfeature",
+        ///             TfsAuthenticationNtlm());
+        ///
+        ///     ReportCodeAnalysisIssuesToPullRequest(
+        ///         MsBuildCodeAnalysis(
+        ///             @"c:\build\msbuild.log",
+        ///             MsBuildXmlFileLoggerFormat),
+        ///         TfsPullRequests(pullRequestSettings),
+        ///         @"c:\repo");
+        /// ]]>
+        /// </code>
+        /// </example>
+        [CakeMethodAlias]
+        [CakeAliasCategory(PullRequestsAliasConstants.PullRequestSystemCakeAliasCategory)]
+        public static IPullRequestSystem TfsPullRequests(
+            this ICakeContext context,
+            TfsPullRequestSettings settings)
+        {
+            context.NotNull(nameof(context));
+            settings.NotNull(nameof(settings));
+
+            return new TfsPullRequestSystem(context.Log, settings);
+        }
+    }
+}
